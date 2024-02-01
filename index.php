b1059bdf79103b846f3432897a14aa0de637eb54
Leah Konma <leahkonma02@gmail.com>
2024-02-01 14:19:50 -0500

    added more files to ignore

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+//this is my controller
 
-//turn on error reporting
+//turn on error reporting always
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -9,13 +10,14 @@ require_once ('vendor/autoload.php');
 
 //instantiate Fat-Free
 $f3 = Base::instance(); //static method
+//java equivalent: Base f3 = new Base();g
 
 //define a default route
 $f3->route('GET /', function (){
-    echo "Week 5";
+//    echo "Week 5";
 
-//    $view = new Template();
-//    echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //run Fat-Free
