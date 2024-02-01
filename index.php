8342972624dd60552b266582d84100e0706b667e
Leah Konma <leahkonma02@gmail.com>
2024-02-01 14:20:21 -0500

    added more variables

--- index.php
+++ index.php
@@ -13,11 +13,25 @@ $f3 = Base::instance(); //static method
 //java equivalent: Base f3 = new Base();g
 
 //define a default route
-$f3->route('GET /', function (){
-//    echo "Week 5";
+$f3->route('GET /', function ($f3){
 
+    //add key-value pairs to the F3 "hive"
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password1'));
+    $f3->set('title', 'Working with Templates');
+
+    //save variables
+    $f3->set('temp', 67);
+    $f3->set('color', 'purple');
+    $f3->set('fruits',array('apple','oranges','banana'));
+    $f3->set('vegetables',array('bokchoy','potatoes','carrots'));
+
+    //display the view page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/info.html');
+
+    //alternate syntax
+//    echo Template::instance()->render('views/info.html');
 });
 
 //run Fat-Free
